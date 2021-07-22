e083a1523a58fef39571de90201d765d136eb2df
류선영 <84279479+tjsdud594@users.noreply.github.com>
2021-07-22 17:52:13 +0900

    first

--- test_distribute/dao.py
+++ test_distribute/dao.py
@@ -7,15 +7,12 @@ class Database():
             conn = pymysql.connect(host='localhost', user='root', password='1234', db='playdata', charset='utf8')
             cursor = conn.cursor()
             
-            try:
-                data = []
-                cursor.execute("select * from emp")
-                rows = cursor.fetchall()
-                print(rows)
-                data.append({"empno" : rows[0], "ename" : rows[1], "job" : rows[2], "mgr" : rows[3], "hiredate" : rows[4], "sal" :rows[5], "comm" : rows[6], "deptno" : rows[7]})
-            
-            except Exception as e:
-                print(e)
+            data = []
+            cursor.execute("select * from emp")
+            rows = cursor.fetchall()
+            print(rows)
+            data.append({"empno" : rows[0], "ename" : rows[1], "job" : rows[2], "mgr" : rows[3], "hiredate" : rows[4], "sal" :rows[5], "comm" : rows[6], "deptno" : rows[7]})
+
             
         except Exception as e:
             print(e)
