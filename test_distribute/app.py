6be1ef417cc931acd586d00d7763060bbeccba4c
류선영 <84279479+tjsdud594@users.noreply.github.com>
2021-07-22 18:15:25 +0900

    first

--- test_distribute/app.py
+++ test_distribute/app.py
@@ -5,11 +5,15 @@ from dao import Database
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def usercheck():
+def get():
 
-    data = (id,)
-    print(data)
+    return render_template("index.html")
+
+@app.route('/show', methods=['GET'])
+def usercheck():
+    print("app")
     return Database.selectdb()
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    # app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='127.0.0.1', port=5000)
--- test_distribute/dao.py
+++ test_distribute/dao.py
@@ -4,14 +4,18 @@ import json
 class Database():
     def selectdb():
         try:
-            conn = pymysql.connect(host='localhost', user='root', password='1234', db='playdata', charset='utf8')
+            # conn = pymysql.connect(host='데이터베이스 엔드포인트(외부에 알려지면 안됨!)', user='root', password='1234', db='playdata', charset='utf8')
+            conn = pymysql.connect(host='database-3.caefhfvxhncb.ap-northeast-2.rds.amazonaws.com', user='admin', password='playdata', db='playdata', charset='utf8')
             cursor = conn.cursor()
             
             data = []
             cursor.execute("select * from emp")
             rows = cursor.fetchall()
-            print(rows)
-            data.append({"empno" : rows[0], "ename" : rows[1], "job" : rows[2], "mgr" : rows[3], "hiredate" : rows[4], "sal" :rows[5], "comm" : rows[6], "deptno" : rows[7]})
+            for row in rows:
+                print(row)
+            # print(row)
+            # data.append({"empno" : row[0], "ename" : row[1], "job" : row[2], "mgr" : row[3], "hiredate" : row[4], "sal" :row[5], "comm" : row[6], "deptno" : row[7]})
+            data.append({"empno" : row[0], "ename" : row[1], "job" : row[2], "mgr" : row[3], "deptno" : row[7]})
 
             
         except Exception as e:
